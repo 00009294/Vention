1f83241e9ebdec546070f721eddf8a86ac015aea
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-07-28 09:01:43 +0500

    Added DbContext

---
+++ Vention/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vention.Models;
+
+namespace Vention.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+       
+        IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+    }
+}
--- Vention/Program.cs
+++ Vention/Program.cs
@@ -1,12 +1,21 @@
+
+using Microsoft.EntityFrameworkCore;
+using Vention.Data;
+using Vention.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUserService, UserService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
