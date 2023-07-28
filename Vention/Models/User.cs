0af5aee919aacde073093bcec6900b79718debe0
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-07-28 08:20:10 +0500

    Created 'User' model

---
+++ Vention/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Vention.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Age { get; set; } = String.Empty;
+        public string City { get; set; } = String.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+
+    }
+}
